d07004f2516c0f7324c28978bcdc28be53bcf596
Ben Wickens <ben.bullock@pusher.com>
2022-03-17 15:58:35 +0000

    fix comma

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,7 +2,7 @@ const Pusher = require("pusher");
 
 const pusher = new Pusher({
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  key: process.env.NEXT_PUBLIC_APP_KEY
+  key: process.env.NEXT_PUBLIC_APP_KEY,
   secret: process.env.NEXT_PUBLIC_APP_SECRET,
   cluster: process.env.cluster,
   useTLS: true
