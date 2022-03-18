4705c25fc2c0fbf4d511292cef1810d11a5a0143
benw-pusher <47355547+benw-pusher@users.noreply.github.com>
2022-03-18 16:29:19 +0000

    add channels

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
 import '../styles/globals.css'
+import Pusher from 'pusher-js';
+import React, {useState, useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+
+  useEffect(() => {
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+
+    const pusher = new Pusher('aae2d2f9e6404d23f0d6', {
+          authEndpoint: '/api/auth',
+          cluster: 'eu'
+        });
+        /*
+    const pusher = new Pusher(process.env.NEXT_PUBLIC_APP_KEY, {
+      authEndpoint: '/api/auth',
+      cluster: process.env.NEXT_PUBLIC_APP_CLUSTER
+    }); */
+
+
+
+
+
+    const channel = pusher.subscribe('private-my-channel');
+    channel.bind('my-event', function(data) {
+      alert(JSON.stringify(data));
+    });
+
+      return () => {
+        pusher.disconnect();
+      };
+    }, []);
+
+    return <Component {...pageProps} />
 }
+
+
 
 export default MyApp
